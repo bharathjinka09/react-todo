0ca46bbd66f97d345e771ab6a8339d8e6d9c78b6
bharathjinka09 <bharathjinka09@gmail.com>
2020-04-28 14:59:14 +0530

    Updates

---
+++ precache-manifest.ee7c87fc2ee7299929e158a43855819c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc132527e805a98fe4ad9b9fb2b1062",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "e6d769a8dcc030e4a478",
+    "url": "/react-todo/static/css/main.19638c5d.chunk.css"
+  },
+  {
+    "revision": "bf24c2d478d86232c17b",
+    "url": "/react-todo/static/js/2.87135e56.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo/static/js/2.87135e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d769a8dcc030e4a478",
+    "url": "/react-todo/static/js/main.9ea12867.chunk.js"
+  },
+  {
+    "revision": "21fa4905a10ae1f3893f",
+    "url": "/react-todo/static/js/runtime-main.9b1518db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
